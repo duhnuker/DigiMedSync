ef32c187ac2f324362c08168916eff333d8b0d79
Dinuka <dinukanh@gmail.com>
2024-07-12 16:31:30 +1000

    database connection fix

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 import 'dotenv/config';
-//environment variables to automatically update port values
+//environment variables to automatically update port values and import database config
 import express from "express";
-import db from './db/index.js';
+import pg from "pg";
+
+const db = new pg.Client();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+db.connect();
+
 // Get home page
-app.get("/", (req, res) => {
-})
+app.get("/", async (req, res) => {
+  console.log("Hello");
+  res.status(200).json({
+    status: "success",
+    data: {
+      random: ["I am Dinuka"],
+    },
+  });
+});
 
 
 app.listen(port, () => {
